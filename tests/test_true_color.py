39a3e609e6f9aadba4f4f46ca38285032bd26f7a
benhid <antoniobenitezhid@gmail.com>
2022-02-16 11:39:45 +0100

    Add new true color test

--- tests/test_true_color.py
+++ tests/test_true_color.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
+import numpy as np
 from typer.testing import CliRunner
 
 from greensenti import app
 from greensenti.cli.compute_index import app as compute_index_group
+from greensenti.cli.compute_index import true_color
 
 runner = CliRunner()
 
@@ -11,9 +15,14 @@ def test_true_color_group():
     assert result.exit_code == 0
 
 
-def test_true_color():
+def test_true_color_cli():
     result = runner.invoke(
         compute_index_group,
         ["true-color", "tests/data/B1.jp2", "tests/data/B2.jp2", "tests/data/B3.jp2"],
     )
     assert result.exit_code == 0
+
+
+def test_true_color():
+    tc = true_color(r=Path("tests/data/B1.jp2"), g=Path("tests/data/B2.jp2"), b=Path("tests/data/B3.jp2"), output=None)
+    assert np.array_equal(tc, np.array([[[85, 170, 255]]]))
