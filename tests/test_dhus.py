6e85f683d1dd5ec89e5f8b7f4b7041ab953cdc27
jfaldanam <jfaldanam@gmail.com>
2022-11-07 23:12:03 +0000

    chore: format tests

--- tests/test_dhus.py
+++ tests/test_dhus.py
@@ -5,22 +5,30 @@ from pandas.testing import assert_frame_equal
 
 from greensenti import dhus
 
+
 def test_gcloud_path_is_valid():
     gcloud_path = dhus.get_gcloud_path("S2B_MSIL2A_20221005T105819_N0400_R094_T30SUF_20221005T135951")
     assert gcloud_path == "L2/tiles/30/S/UF/S2B_MSIL2A_20221005T105819_N0400_R094_T30SUF_20221005T135951.SAFE"
 
+
 def test_copernicous_download_returns_correct_dataframe():
     # Mock sentinelsat.sentinel.SentinelAPI.download_all
-    mock = MagicMock() 
-    mock.download_all.return_value= (
-            ["uuid1", "uuid5"],
-            ["uuid2"],
-            ["uuid3", "uuid4"],
-        )
+    mock = MagicMock()
+    mock.download_all.return_value = (
+        ["uuid1", "uuid5"],
+        ["uuid2"],
+        ["uuid3", "uuid4"],
+    )
 
     status_df = dhus.copernicous_download(
-        ids = ["uuid1", "uuid2", "uuid3", "uuid4", "uuid5",],
-        api = mock,
+        ids=[
+            "uuid1",
+            "uuid2",
+            "uuid3",
+            "uuid4",
+            "uuid5",
+        ],
+        api=mock,
     )
 
     expected_status = pd.DataFrame(
@@ -34,4 +42,3 @@ def test_copernicous_download_returns_correct_dataframe():
     )
 
     assert_frame_equal(status_df, expected_status)
-
